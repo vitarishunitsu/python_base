8177f49b818cca2af5486073866c15f238a62c0f
Vitaly Panteleev <vitaly.insyslab@gmail.com>
2019-12-10 17:50:06 +0300

    lesson 2

--- lesson2/ex6.py
+++ lesson2/ex6.py
@@ -1,20 +1,21 @@
-#Торопился, поэтому обработку ввода не сделал. Её вообще уде пора через функции реализовывать...
+# Торопился, поэтому обработку ввода не сделал. Её вообще уде пора через функции реализовывать...
 product_list = []
 
 for i in range(3):
-    print(f'Товар № {i+1}')
+    print(f'Товар № {i + 1}')
     name = input('Введите название товара: ')
     cost = input('Введите цену товара: ')
     qty = input('Введите количество товара: ')
     unit = input('Введите единицу измерения товара: ')
-    product = {'название': name, 'цена': cost, 'количество' : qty, 'ед.изм.': unit}
+    product = {'название': name, 'цена': cost, 'количество': qty, 'ед.изм.': unit}
     product_list.append((i + 1, product))
 
 print('Статистика: ')
 
-stat_product = {'название': [], 'цена': [], 'количество' : [], 'ед.изм.': []}
-
+print(product_list)
+stat_product = dict.fromkeys(product.keys(), [])
+print(stat_product)
 for el in product_list:
     for key in el[1]:
-        stat_product[key].append(el[1][key])
-print(stat_product)
+        stat_product[key].append(el[1].get(key))
+
