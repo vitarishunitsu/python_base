3b9a922790fd47fc0a27715b364b1e93551e82a3
Vitaly Panteleev <vitaly.insyslab@gmail.com>
2019-12-27 15:35:45 +0300

    Lesson 7

---
+++ lesson7/ex1.py
@@ -0,0 +1,30 @@
+class Matrix:
+    def __init__(self, inp_list):
+        self.data = inp_list
+
+    def __str__(self):
+        return f'{self.data}'
+
+    def __add__(self, other):
+        new_matrix = self.data[:]
+        for column in range(len(new_matrix)):
+            for ind in range(len(new_matrix[column])):
+                new_matrix[column][ind] += other.data[column][ind]
+        return Matrix(new_matrix)
+
+
+my_list_1 = [
+    [1, 2, 3],
+    [1, -2, 3],
+    [0, 2, 15],
+]
+
+my_list_2 = [
+    [6, 2],
+    [2, -2, 3],
+    [1, 2, 15],
+]
+
+my_matrix_1 = Matrix(my_list_1)
+my_matrix_2 = Matrix(my_list_2)
+print(my_matrix_1 + my_matrix_2)
